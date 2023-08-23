0127e516dbbcc96a0d1d2be59f1e43b9b5dddc1b
Matias Blanco <matias.david.blanco@gmail.com>
2023-08-23 15:07:46 -0300

    corrigiendo método para navegar a pagina del reg.

--- force-app/main/default/lwc/listaTipos_Tanques/listaTipos_Tanques.js
+++ force-app/main/default/lwc/listaTipos_Tanques/listaTipos_Tanques.js
@@ -2,38 +2,22 @@ import { LightningElement, wire, api } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation'; 
 import getAllTiposDeTanque from "@salesforce/apex/tiposDeTanquesService.getAllTiposDeTanque";
 
-export default class ListaTipos_Tanques extends LightningElement {
-    @api TipoDeTanque;
+export default class ListaTipos_Tanques extends NavigationMixin (LightningElement) {
+    //@api TipoDeTanque;
     @wire(getAllTiposDeTanque) ListaTiposTanques;
     
 
-    navigateToRecordViewPage(e) {
-        console.log("Button clicked");
-        console.log(this.ListaTiposTanques.data[0].Id);
-       
-
-        const recordId = e.target.dataset.id;
-        console.log("Button clicked with record ID:", recordId);
-
-        const event = new CustomEvent('verdetalles', {
-            detail: {
-                recordId: recordId
-            }
-        });
-        this.dispatchEvent(event);
-
-        // View a custom object record.
+    navigateToRecordViewPage(event) {
+        const recordId = event.currentTarget.dataset.id;
+        console.log(recordId);
         this[NavigationMixin.Navigate]({
             type: 'standard__recordPage',
             attributes: {
-                recordId:  this.recordId,
+                recordId:  recordId,
                 objectApiName: 'Tipo_de_Tanque__c', // objectApiName is optional
                 actionName: 'view'
             }
         });
     }
-
-
-
-
 }
+
